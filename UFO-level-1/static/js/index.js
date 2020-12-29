fe866760f01996e9b9f06a7bbb4f76cfd4717b7f
Sarah Heavner <sheavner@gmail.com>
2020-12-28 17:28:51 -0700

    updated code to use fat arrow on table data

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -4,40 +4,14 @@ var tableData = data;
 // get reference to table body
 var tbody = d3.select("tbody");
 
-// console.log data from data.js
-console.log(tableData);
 
-// Loop through data and console.log each ufo observation object
-tableData.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
-});
-
-// append table row for each Object
-tableData.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
+// Loop through data.js, collect keys and values from each Object and append to table 
+tableData.forEach((ufoSightings) => {
     var row = tbody.append("tr");
-
-});
-
-// Use `Object.entries` to console.log each weather report value
-tableData.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
-    var row = tbody.append("tr");
-
-    Object.entries(ufoSightings).forEach(function([key, value]) {
-        console.log(key, value);
-    });
-});
-
-// Use d3 to update each cell's text
-tableData.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
-    var row = tbody.append("tr");
-
-    Object.entries(ufoSightings).forEach(function([key, value]) {
+    Object.entries(ufoSightings).forEach(([key, value]) => {
         console.log(key, value);
 
-        var cell = row.append("td")
-        cell.text(value);
+        var cell = row.append("td");
+        cell.text(value);   
     });
 });
